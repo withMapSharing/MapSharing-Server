518b659c864e8287f2bbd5e7c1df1b1d9c22ed27
Heedong Kim <ruthetum@gmail.com>
2020-12-04 12:33:33 +0900

    [feat] 기본 라우팅 설정

---
+++ routes.js
@@ -0,0 +1,16 @@
+// Users
+const USERS = "/users";
+
+// Maps
+const MAPS = "/maps";
+
+// Places
+const PLACES = "/places";
+
+const routes = {
+  users: USERS,
+  maps: MAPS,
+  places: PLACES
+};
+
+export default routes;
